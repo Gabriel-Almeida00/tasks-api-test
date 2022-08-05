aaa0a00fbcc999f99cb494a6dbd7c13ec7beca84
gabriel <gaamdal557@gmail.com>
2022-08-05 15:48:31 -0300

    first commit

---
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -0,0 +1,41 @@
+package br.ce.wcaquino.tasks.apitest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class APITest {
+
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8080/tasks-backend";
+	}
+
+	@Test
+	public void deveRetornarTarefas() {
+		RestAssured.given().when().get("/todo").then().statusCode(200);
+	}
+
+	@Test
+	public void deveadcionarTarefaComSucesso() {
+		RestAssured.given()
+		.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2022-08-30\" }")
+		.contentType(ContentType.JSON)
+		.when().post("/todo")
+		.then().statusCode(201);
+	}
+	
+	@Test
+	public void naoDeveAdcionarTarefaInvalida() {
+		RestAssured.given()
+		.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2010-08-30\" }")
+		.contentType(ContentType.JSON)
+		.when().post("/todo")
+		.then().statusCode(400)
+		.body("message", CoreMatchers.is("Due date must not be in past"))
+		;
+	}
+}
